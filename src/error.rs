70076ce5d2a46054900941e978f1e8f66c4c0bdd
eareimu <reimu@genmeta.net>
2026-03-30 16:10:48 +0800

    fix(error): add Send + Sync bounds to Whatever source

--- src/error.rs
+++ src/error.rs
@@ -30,8 +30,8 @@ pub enum Error {
     #[snafu(whatever, display("{message}"))]
     Whatever {
         message: String,
-        #[snafu(source(from(Box<dyn std::error::Error>, Some)))]
-        source: Option<Box<dyn std::error::Error>>,
+        #[snafu(source(from(Box<dyn std::error::Error + Send + Sync>, Some)))]
+        source: Option<Box<dyn std::error::Error + Send + Sync>>,
     },
 }
 
